5108af37940fe4aa32071fe50f93bfbc0739655b
piood <fsssggggoood@gmail.com>
2022-06-12 11:21:17 +0800

    Deque interface

--- proj1b/Deque.java
+++ proj1b/Deque.java
@@ -1,10 +1,17 @@
-public interface Deque {
+public interface Deque<T> {
     void addFirst(T item);
+
     void addLast(T item);
+
     boolean isEmpty();
+
     int size();
+
     void printDeque();
+
     T removeFirst();
+
     T removeLast();
+
     T get(int index);
 }
